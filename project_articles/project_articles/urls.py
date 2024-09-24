4f2d8c21d4c34fc83adb98c669d918a538624830
Shamil <sh.gabaev@gmail.com>
2024-09-24 17:47:21 +0500

    added new app - webapp

--- project_articles/project_articles/urls.py
+++ project_articles/project_articles/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from webapp import views as webapp_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', webapp_views.index_view, name='home')
 ]
